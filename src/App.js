2931e6928997aea993ecc0425514b7c0cb4bd01f
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-09-12 23:44:41 +0530

    Basic text laid out on page.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-    return <h1>Reusable Tooltip Project</h1>;
+    return (
+        <div>
+            <h1>Reusable Tooltip Project</h1>
+            <section>
+                <p>Introductory text</p>
+            </section>
+            <section>
+                <h2>Demo Area 1</h2>
+                <div>Something here</div>
+            </section>
+            <section>
+                <h2>Demo Area 2</h2>
+                <div>Something here</div>
+            </section>
+        </div>
+    );
 }
 
 export default App;
