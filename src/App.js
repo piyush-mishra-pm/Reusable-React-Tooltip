047e584e2d03ecf739ce547c6ed23973cc0c4bf9
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-09-14 23:09:12 +0530

    Added CSS and HTML, for decription. Can fill in examples now.

--- src/App.js
+++ src/App.js
@@ -20,29 +20,59 @@ function App() {
 
     return (
         <div>
-            <h1>Reusable Tooltip Project</h1>
-            <section>
-                <p>Introductory text</p>
-                <p>What tooltip to show? Leave empty for default tooltip text.</p>
-                <InputText onTextChange={onTooltipTextChange} />
-                <p>And direction</p>
-                <DropdownMenu onDirectionChange={setDirection} />
-            </section>
-            <section>
-                <h2>Demo Area 1</h2>
+            <header className="flex-vertical">Reusable React Tooltips</header>
+            <section className="section-with-boundary flex-vertical">
+                <div className="text-heading">Introductory text</div>
                 <div>
-                    <p>baked tooltip text</p>
-                    <Tooltip content="Tooltip Baked content" direction="top">
-                        Something here
-                    </Tooltip>
+                    <p>
+                        A simple, reusable react tooltip. Can be wrapped around any HTML / JSX entity, and given custom
+                        tooltip text, position (top, down, right, left), and appearance delay. This can be by baking
+                        these in code (when wrapping the elements), or using event handlers to dynamically provide these
+                        values.
+                    </p>
+                    <p>Find below, examples of different elements using these tooltips in different ways.</p>
                 </div>
             </section>
-            <section>
-                <h2>Demo Area 2</h2>
+            <section className="section-with-boundary flex-vertical">
+                <div className="text-heading">Static Baked tooltips</div>
                 <div>
+                    <p>baked tooltip text</p>
+                    <div className="flex-horizontal">
+                        <div className="flex-vertical section-with-boundary">
+                            <Tooltip content="Tooltip Baked content" direction="top">
+                                Something here
+                            </Tooltip>
+                        </div>
+                        <div className="flex-vertical section-with-boundary">
+                            <Tooltip content="Tooltip Baked content" direction="top">
+                                Something here
+                            </Tooltip>
+                        </div>
+                        <div className="flex-vertical section-with-boundary">
+                            <Tooltip content="Tooltip Baked content" direction="top">
+                                Something here
+                            </Tooltip>
+                        </div>
+                        <div className="flex-vertical section-with-boundary">
+                            <Tooltip content="Tooltip Baked content" direction="top">
+                                Something here
+                            </Tooltip>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="section-with-boundary flex-vertical">
+                <div className="text-heading">Dynamic Tooltips</div>
+                <div>
+                    <div>
+                        <p>What tooltip to show? Leave empty for default tooltip text.</p>
+                        <InputText onTextChange={onTooltipTextChange} />
+                        <p>And direction</p>
+                        <DropdownMenu onDirectionChange={setDirection} />
+                    </div>
                     <p>Dynamic tooltip text</p>
                     <Tooltip content={tooltipText} direction={direction}>
-                        Something here
+                        <p className="hoverable">Something here</p>
                     </Tooltip>
                 </div>
             </section>
